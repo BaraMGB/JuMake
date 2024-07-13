74457f1b60da660ae227d4cdb8b692d05f32a960
Steffen Baranowsky <BaraMGB@arcor.de>
2024-07-14 00:27:27 +0200

    create project with CMakeLists.txt and scr folder with main.cpp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::{fs, io::Result, path::Path};
 use clap::{Parser, Subcommand};
+use std::fs;
+use std::fs::File;
+use std::path::Path;
+use std::io::Write;
+use indoc::indoc;
 
 
 #[derive(Parser)]
@@ -41,8 +45,7 @@ fn main() {
 
     match &cli.command {
         Commands::New { project_name, path } => {
-            println!("Creating new project '{}'...", project_name);
-            // Here comes the logic for creating the project
+            create_project(project_name.clone(), path.clone());
         }
         Commands::Add { class_name, header_only, output } => {
             println!("Adding class '{}'...", class_name);
@@ -57,4 +60,50 @@ fn main() {
             // Here comes the logic for building the project
         }
     }
+}
+
+fn create_project(projectname: String, path: Option<String>) {
+    let project_path = match path {
+        Some(p) => Path::new(&p).join(&projectname),
+        None => Path::new(&projectname).to_path_buf(),
+    };
+
+    if project_path.exists() {
+        println!("Error: Project directory already exists: {}", project_path.display());
+        return;
+    }
+
+    println!("Creating project '{}' at {}...", projectname, project_path.display());
+
+    // Create project directory
+    fs::create_dir_all(&project_path).expect("Failed to create project directory");
+
+
+    // TODO:
+    // 1. create CMakeLists.txt
+
+    let cmakelists_path = project_path.join("CMakeLists.txt");
+    let mut cmakelists_file = File::create(cmakelists_path).expect("Failed to create CMakeLists.txt");
+    let cmake_content = format!(
+        indoc! {
+            "cmake_minimum_required(VERSION 3.24)
+             project({})
+
+             # Add your project's source files and targets here"
+        },
+        projectname
+    );
+    cmakelists_file
+        .write_all(cmake_content.as_bytes())
+        .expect("Failed to write to CMakeLists.txt");
+    // 2. create source directory and template filed
+    let src_path = project_path.join("src");
+    fs::create_dir(&src_path).expect("Failed to create src directory");
+
+    // Create main.cpp
+    let main_cpp_path = src_path.join("main.cpp");
+    let mut main_cpp_file = File::create(main_cpp_path).expect("Failed to create main.cpp");
+    main_cpp_file.write_all(b"// Your code goes here!").expect("Failed to write to main.cpp");
+    // TODO: Add more template files
+    // 3. Initialize Git repo
 }
